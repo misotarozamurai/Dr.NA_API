65e4b367ab14d8b978213a5e4f8beabb3f461d19
Aoi Satonaka <fireball0821@icloud.com>
2020-01-18 05:17:13 +0900

    [update] Util.log() has more easily tracing

--- utils/util.js
+++ utils/util.js
@@ -1,1 +1,11 @@
-export const log = str => console.log((new Date).toString() + '\n"' + str + '"\n');
+export default class Util {
+    static log (...messages) {
+       (function expantion(messages,count = 0) {
+            messages.forEach(msg => {
+                Array.isArray(msg)
+                    ? expantion(msg,++count)
+                    : console.log('\t'.repeat(count) + msg);
+            });
+        })([(new Date).toString(),messages]);
+    }
+}
